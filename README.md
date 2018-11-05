17b0402413fdf410d188ad502ded60857d2b71e7
ilsee(김일인) <ilsee@coupang.com>
2018-11-05 17:54:34 +0900

    write test code

--- README.md
+++ README.md
@@ -9,3 +9,53 @@
 - Notify.js
 - Spock Test Framework
 - H2 Database
+
+
+## Execute on IntelliJ
+- Preferences > Build, Execution, Deployment > Compiler > Annotation Processors > Check 'Enable annotation processing'
+- Project Structure > Project > Set Java version to '1.8'
+- Execute 'ScheduleManagerApplication.java'
+
+## Execute on Terminal
+~~~
+$ cd {project root}
+$ ./gradlew build
+$ java -jar build/libs/schedule-manager-0.0.1-SNAPSHOT.jar
+~~~
+## Functions
+- Make Reservation
+  - First way: Click 'Make Reservation' Button (needs to put all required inputs)
+  - Second way: Drag & Drop on Calendar (automatically set 'start' and 'end' time)
+- Manage Room
+  - Click 'Manage Room' category on navigation bar
+  - Put 'Room Name'
+  - Select 'Room Color'
+  - Click 'Save' Button
+  - You can see different colors for each conference room on calendar
+  
+## Entity Relationship
+~~~
+- ReservationGroup
+  - Reservation
+    - ReservationCell
+      - Room
+      - Timeline
+~~~     
+ReservationGroup: 반복하는 예약들의 집합
+Reservation: 예약. 여러 ReservationCell을 가짐
+ReservationCell: 30분 단위의 예악 cell
+Room: 컨퍼런스 룸
+TimeLine: 30분단위의 시간 집합 테이블
+
+## 문제 해결
+1. 동시성 및 중복 처리
+  - 고정된 TimeLine 테이블에 30분간격의 시간데이터를 갖고 있음.
+  - ReservationCell에 date, room_id, time_line_id 필드를 Unique Key로 지정 하여 동시에 동일한 room과 동일한 예약시간으로 들어온 예약을 방지함
+  
+2. 반복되는 Reservation에 대한 관리
+  - ReservationGroup을 두어 반복 Reservation들에 대한 동일한 처리를 할 수 있도록 설계함.
+
+   
+  
+  
+
--- src/test/groovy/com/ilsee/schedulemanager/controller/converter/ReservationGroupConverterTest.groovy
+++ src/test/groovy/com/ilsee/schedulemanager/controller/converter/ReservationGroupConverterTest.groovy
@@ -1,37 +1,81 @@
-//package com.ilsee.schedulemanager.controller.converter
-//
-//import com.ilsee.schedulemanager.domain.reservation.Reservation
-//import com.ilsee.schedulemanager.domain.room.Room
-//import com.ilsee.schedulemanager.domain.room.RoomRepository
-//import com.ilsee.schedulemanager.domain.timeline.TimeLine
-//import com.ilsee.schedulemanager.domain.timeline.TimeLineRepository
-//import spock.lang.Specification
-//import spock.lang.Unroll
-//
-//import java.time.LocalTime
-//
-//class ReservationGroupConverterTest extends Specification {
-//    @Unroll
-//    def "Reservation Group 생성: #DESC"() {
-//        setup:
-//        def roomRepository = Mock(RoomRepository) {
-//            findById(_) >> new Room(id: 1L, roomName: "테스트 미팅룸", roomColor: "#696C85")
-//        }
-//        def timeLineRepository = Mock(TimeLineRepository) {
-//            findAllByStartGreaterThanEqual(_) >> [new TimeLine(id: 1L, start: LocalTime.of(23,30), end: LocalTime.of(0,0))]
-//            findAllByStartGreaterThanEqualAndEndLessThanEqual(_,_) >> [new TimeLine(id: 1L, start: LocalTime.of(23,0), end: LocalTime.of(23,30))]
-//        }
-//
-//        ReservationGroupConverter converter = new ReservationGroupConverter(roomRepository: roomRepository, timeLineRepository: timeLineRepository)
-//
-//        when:
-//        def result = converter.apply(RESERVATION_DTO)
-//
-//        then:
-//        result.reservationList.equals(new Reservation(reservationCellList: RESULT_CELL_LIST))
-//
-//        where:
-//        DESC | RESERVATION_DTO | RESULT_CELL_LIST
-//
-//    }
-//}
+package com.ilsee.schedulemanager.controller.converter
+
+import com.ilsee.schedulemanager.controller.api.reservation.ReservationRequestDto
+import com.ilsee.schedulemanager.domain.reservation.Reservation
+import com.ilsee.schedulemanager.domain.reservationcell.ReservationCell
+import com.ilsee.schedulemanager.domain.room.Room
+import com.ilsee.schedulemanager.domain.room.RoomRepository
+import com.ilsee.schedulemanager.domain.timeline.TimeLine
+import com.ilsee.schedulemanager.domain.timeline.TimeLineRepository
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+class ReservationGroupConverterTest extends Specification {
+    RoomRepository roomRepository = Mock(RoomRepository) {
+        findById(_) >> Optional.of(new Room(id: 1L, roomName: "테스트 미팅룸", roomColor: "#696C85"))
+    }
+    TimeLineRepository timeLineRepository = Mock(TimeLineRepository) {
+        findAllByStartGreaterThanEqual(_) >>
+                [new TimeLine(id: 1L, start: LocalTime.of(23,0), end: LocalTime.of(23,30)),
+                 new TimeLine(id: 2L, start: LocalTime.of(23,30), end: LocalTime.of(0,0))]
+        findAllByStartGreaterThanEqualAndEndLessThanEqual(_,_) >>
+                [new TimeLine(id: 1L, start: LocalTime.of(23,0), end: LocalTime.of(23,30)),
+                 new TimeLine(id: 2L, start: LocalTime.of(23,30), end: LocalTime.of(0,0))]
+    }
+    @Unroll
+    def "Reservation Group 반복예약 테스트: #DESC"() {
+        setup:
+        ReservationGroupConverter converter = new ReservationGroupConverter(roomRepository,timeLineRepository)
+
+        when:
+        def result = converter.apply(new ReservationRequestDto(
+                title: "테스트",
+                userName:"테스트유저",
+                roomId: 1L,
+                repeatCount: REPEAT_COUNT,
+                date: LocalDate.now(),
+                startTime: LocalTime.of(23, 30),
+                endTime: LocalTime.of(0,0)))
+
+        then:
+		result.reservationList.size() == RESERVATION_LIST_SIZE
+
+        where:
+        DESC | REPEAT_COUNT | RESERVATION_LIST_SIZE
+		"반복예약 테스트: 1개 예약" | 1 | 1
+		"반복예약 테스트: 2개 예약" | 2 | 2
+    }
+
+    @Unroll
+    def "Reservation Group 생성 테스트: #DESC"() {
+        setup:
+        ReservationGroupConverter converter = new ReservationGroupConverter(roomRepository,timeLineRepository)
+
+        when:
+        def result = converter.apply(new ReservationRequestDto(
+                title: "테스트",
+                userName:"테스트유저",
+                roomId: 1L,
+                repeatCount: 1,
+                date: LocalDate.now(),
+                startTime: LocalTime.of(23, 0),
+                endTime: END_TIME))
+
+        then:
+        result.reservationList.iterator().next() == new Reservation(reservationCellList: RESULT_CELL_LIST)
+
+        where:
+        DESC | END_TIME | RESULT_CELL_LIST
+        "예약시간이 23:00 ~ 00:00 일 경우, 23:00 ~ 00:00 2개 cell 생성" |
+                LocalTime.of(0,0) |
+                [new ReservationCell(room: new Room(id: 1L), date: LocalDate.now(), timeLine: new TimeLine(id: 1L)),
+                 new ReservationCell(room: new Room(id: 1L), date: LocalDate.now(), timeLine: new TimeLine(id: 2L))]
+        "예약시간이 23:00 ~ 23:30 일 경우, 23:00~23:30 1개 cell 생성" |
+                LocalTime.of(23,30) |
+                [new ReservationCell(room: new Room(id: 1L), date: LocalDate.now(), timeLine: new TimeLine(id: 1L))]
+    }
+
+}
